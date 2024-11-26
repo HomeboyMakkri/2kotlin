6e790bf008735d27012082b28ca74b03799e3797
makarlistkov <makarmakaroshkin@mail.ru>
2024-11-26 17:51:33 +0300

    del comm

--- app/src/main/java/com/example/a2kotlin/GiphyApiService.kt
+++ app/src/main/java/com/example/a2kotlin/GiphyApiService.kt
@@ -4,13 +4,13 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-// API Interface
+
 interface GiphyApi {
     @GET("random?api_key=Koj7rrZfxPMmbim30u9gY4okmFmMmCqT&tag=&rating=g")
     suspend fun getRandomGif(): GifResponse
 }
 
-// Data models
+
 data class GifResponse(val data: GifData)
 data class GifData(val images: GifImages)
 data class GifImages(val original: GifOriginal)
--- app/src/main/java/com/example/a2kotlin/MainActivity.kt
+++ app/src/main/java/com/example/a2kotlin/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 
-// Main Activity
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                GiphyApp() // Calling composable function inside the setContent block
+                GiphyApp()
             }
         }
     }
